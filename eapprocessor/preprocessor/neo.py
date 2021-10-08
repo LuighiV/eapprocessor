65d6c6994f1c67cf0972ecfab89d6b7a4e46afee
Luighi <luighiavz@gmail.com>
2021-10-08 12:02:33 -0500

    Change NEO to matrix operation

--- eapprocessor/preprocessor/neo.py
+++ eapprocessor/preprocessor/neo.py
@@ -5,9 +5,17 @@ import numpy as np
 def apply_neo_to_array(values, w=1):
     print("Apply to recording with w=", w)
     newarray = np.array(values)
-    for idx in range(len(values))[w:-w]:
-        newarray[idx] = values[idx]**2 - values[idx + w] * values[idx - w]
-    return newarray
+    # for idx in range(len(values))[w:-w]:
+    #     newarray[idx] = values[idx]**2 - values[idx + w] * values[idx - w]
+    # return newarray
+
+    operation = (newarray**2 -
+                 np.roll(newarray, w) * np.roll(newarray, -w))
+
+    mask = (np.arange(len(newarray)) >= w) * (np.arange(len(newarray))
+                                              < len(newarray) - w)
+
+    return operation * mask + newarray * (~mask)
 
 
 if __name__ == "__main__":
